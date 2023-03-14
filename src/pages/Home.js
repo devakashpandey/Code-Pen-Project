936a7221e9732be61700877532a310ef6648cec6
DevAkky <developerakky@gmaiil.com>
2023-03-14 16:16:44 +0530

    folder & file structure

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../components/Header";
+import Code from "../components/Code";
+import Result from "../components/Result";
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Code />
+      <Result />
+    </>
+  );
+};
+
+export default Home;
