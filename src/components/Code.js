448b9d85cbebc11c6fb0e21404a4702f9ec484cf
DevAkky <developerakky@gmaiil.com>
2023-03-14 16:25:41 +0530

    editor done

--- src/components/Code.js
+++ src/components/Code.js
@@ -1,7 +1,38 @@
 import React from "react";
+import Editor from "./Editor";
+import { UseDataValue } from "../context/DataProvider";
+import "./Code.css";
 
 const Code = () => {
-  return <div></div>;
+  const { html, setHtml, css, setCss, js, setJs } = UseDataValue();
+
+  return (
+    <>
+      <div className="editor-wrapper">
+        <Editor
+          name="HTML"
+          symbol="/"
+          color="#FF3C41"
+          value={html}
+          onChange={setHtml}
+        />
+        <Editor
+          name="CSS"
+          symbol="*"
+          color="#0EBEFF"
+          value={css}
+          onChange={setCss}
+        />
+        <Editor
+          name="JS"
+          symbol="()"
+          color="#FCD000"
+          value={js}
+          onChange={setJs}
+        />
+      </div>
+    </>
+  );
 };
 
 export default Code;
--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,7 +1,57 @@
-import React from "react";
+import { Box } from "@mui/material";
+import "./Editor.css";
+import CloseFullscreenIcon from "@mui/icons-material/CloseFullscreen";
+import { Controlled as ControlledEditor } from "react-codemirror2"; // editor package
+// editor css
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/material.css";
+// importing editor supportive languages
+import "codemirror/mode/xml/xml";
+import "codemirror/mode/javascript/javascript";
+import "codemirror/mode/css/css";
+import { useState } from "react";
+import SettingsIcon from "@mui/icons-material/Settings";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
-const Editor = () => {
-  return <div></div>;
+const Editor = ({ name, symbol, color, value, onChange }) => {
+  const [toggle, setToggle] = useState(true);
+
+  const handleChange = (editor, data, value) => {
+    onChange(value);
+  };
+
+  return (
+    <>
+      <Box className="container" style={toggle ? null : { flexGrow: 0 }}>
+        <div className="header">
+          <header className="heading">
+            <span className="language" style={{ backgroundColor: color }}>
+              {symbol}
+            </span>
+            &nbsp;&nbsp;{name}
+          </header>
+          <div style={{ marginTop: 7 }}>
+            <SettingsIcon className="editor-icon" />
+            <KeyboardArrowDownIcon className="editor-icon" />
+            <CloseFullscreenIcon
+              className="editor-icon"
+              onClick={() => setToggle((prevState) => !prevState)}
+            />
+          </div>
+        </div>
+
+        <ControlledEditor
+          className="controlled-editor" // default class for extra classname
+          value={value}
+          onBeforeChange={handleChange}
+          options={{
+            theme: "material",
+            lineNumbers: "true", // for showing the numbers
+          }}
+        />
+      </Box>
+    </>
+  );
 };
 
 export default Editor;
