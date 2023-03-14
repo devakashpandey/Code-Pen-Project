6908754a5fc5de0eec3fb4359715d18c91c17b72
DevAkky <developerakky@gmaiil.com>
2023-03-14 16:27:51 +0530

    result box done

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./Result.css";
+import { UseDataValue } from "../context/DataProvider";
 
 const Result = () => {
-  return <div></div>;
+  const [src, setSrc] = useState(""); // this is for set time out
+
+  const { html, css, js } = UseDataValue();
+
+  const srcCode = `
+       <html>
+         <body> ${html}</body>
+         <style>${css} </style>
+         <script>${js} </script>
+       </html>
+    `;
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrc(srcCode);
+    }, 1000);
+
+    return () => clearTimeout(timeout);
+  }, [html, css, js]);
+
+  return (
+    <div className="result-box">
+      <iframe
+        srcDoc={src}
+        title="output"
+        sandbox="allow-scripts"
+        height="100%"
+        width="99.7%"
+        frameBorder={0}
+      />
+    </div>
+  );
 };
 
 export default Result;
